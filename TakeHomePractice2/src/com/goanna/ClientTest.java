2333655ed8f0a403a1115def2535779065e5ae12
Ofego-Goanna <ofego@goanna.group>
2021-07-12 15:41:35 +1000

    take home practice 2

---
+++ TakeHomePractice2/src/com/goanna/ClientTest.java
@@ -0,0 +1,24 @@
+package com.goanna;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientTest {
+
+    @Test
+    void allPositives() {
+    }
+
+    @Test
+    void allEven() {
+    }
+
+    @Test
+    void squared() {
+    }
+
+    @Test
+    void removeNegatives() {
+    }
+}
