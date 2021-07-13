46c446a1b930394103b6003a82e01cc82659785e
indiefernando <indie.fernando@Outlook.com>
2021-07-13 16:00:11 +1000

    Adding changes

--- TakeHomePractice2/src/com/goanna/Client.java
+++ TakeHomePractice2/src/com/goanna/Client.java
@@ -2,61 +2,131 @@ package com.goanna;
 
 import java.util.ArrayList;
 
+import java.util.Iterator;
+
 public class Client {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    }
+	}
 
-    /**
-     *
-     * @param list
-     * @return true if all items in the list are positive
-     * IMPORTANT:
-     * return false if list is null
-     * return true if list is empty
-     */
-    public static boolean allPositives(ArrayList<Integer> list) {
-        if(list == null)
-            return false;
-        for(Integer item: list) {
-            if(item <= 0) { //<= means NOT positive
-                return false;
-            }
-        } //end of loop
-        return true;
-    }
+	/**
+	 *
+	 * 
+	 * 
+	 * @param list
+	 * 
+	 * @return true if all items in the list are positive IMPORTANT: return false if
+	 * 
+	 *         list is null -Done return true if list is empty -Done
+	 * 
+	 */
 
-    /**
-     *
-     * @param list
-     * @return true if all items in the list are even (divisible by 2)
-     * IMPORTANT:
-     * return false if list is null
-     * return true if list is empty
-     */
-    public static boolean allEven(ArrayList<Integer> list) {
-        return false; //to be completed
-    }
+	public static boolean allPositives(ArrayList<Integer> list) {
+		if (list == null)
+			return false;
 
-    //countInRange goes here
+		if (list.isEmpty())
+			return true;
 
-    /**
-     *
-     * @param list
-     * modify the list so that each item becomes the square of its previous value
-     */
-    public static void squared(ArrayList<Integer> list) {
-        //to be completed
-    }
+		for (Integer item : list) {
+			if (item <= 0) { // <= means NOT positive
+				return false;
+			}
+		} // end of loop
+		
+		return true;
+	}
 
-    /**
-     *
-     * @param list
-     * modify the list and remove any negative items
-     */
-    public static void removeNegatives(ArrayList<Integer> list) {
-        //to be completed
-    }
+	/**
+	 *
+	 * 
+	 * 
+	 * @param list
+	 * 
+	 * @return true if all items in the list are even (divisible by 2) IMPORTANT:
+	 * 
+	 *         return false if list is null return true if list is empty
+	 * 
+	 */
+
+	public static boolean allEven(ArrayList<Integer> list) {
+		if (list == null)
+			return false;
+
+		if (list.isEmpty())
+			return true;
+
+		for (Integer item : list) {
+			if (item % 2 != 0) { // <= means NOT Even
+				return false;
+			}
+
+		} // end of loop
+
+		return true;
+	}
+
+	// countInRange goes here
+
+	/**
+	 *
+	 * 
+	 * 
+	 * @param list modify the list so that each item becomes the square of its
+	 * 
+	 *             previous value
+	 * 
+	 */
+
+	public static void squared(ArrayList<Integer> list) {
+		
+		for (int i = 0; i < list.size(); i++) {
+			list.set(i, (int) Math.pow(list.get(i), 2));
+			
+		}
+	}
+
+	/**
+	 *
+	 * 
+	 * 
+	 * @param list modify the list and remove any negative items
+	 * 
+	 */
+
+	public static void removeNegatives(ArrayList<Integer> list) {
+
+		Iterator<Integer> iterator = list.iterator();
+
+		while (iterator.hasNext()) {
+			if (iterator.next() < 0)
+				iterator.remove();
+		}
+	}
+	
+		public static void removePositives(ArrayList<Integer> list) {
+
+			Iterator<Integer> iterator = list.iterator();
+
+			while (iterator.hasNext()) {
+				if (iterator.next() > 0)
+					iterator.remove();
+			}	
+		
+		
+		/*
+		 * 
+		 * for (int i = 0; i < list.size(); i++) {
+		 * 
+		 * if (list.get(i) <= 0) // <= means NOT positive
+		 * 
+		 * list.remove(list.get(i));
+		 * 
+		 * } // end of loop
+		 * 
+		 */
+
+	}
 
 }
--- TakeHomePractice2/src/com/goanna/ClientTest.java
+++ TakeHomePractice2/src/com/goanna/ClientTest.java
@@ -1,24 +1,81 @@
 package com.goanna;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class ClientTest {
 
-    @Test
-    void allPositives() {
-    }
+	ArrayList<Integer> list;
 
-    @Test
-    void allEven() {
-    }
+	@BeforeEach
+	void Init() {
+		list = new ArrayList<Integer>();
+	}
 
-    @Test
-    void squared() {
-    }
+	@Test
+	void testAllPositives() {
+		list.add(22);
+		list.add(12);
+		list.add(-10);
+		list.add(0);
 
-    @Test
-    void removeNegatives() {
-    }
+		assertFalse(Client.allPositives(list));
+	}
+
+	@Test
+	void testAllEven() {
+		list.add(2);
+		list.add(2);
+		list.add(4);
+		list.add(6);
+
+		assertTrue(Client.allEven(list));
+	}
+
+	@Test
+	void testSquared() {
+		list.add(1);
+		list.add(0);
+		list.add(7);
+		list.add(-2);
+		list.add(9);
+
+		Client.squared(list);
+		assertEquals("[1, 0, 49, 4, 81]", list.toString());
+	}
+
+	@Test
+	void testRemoveNegatives() {
+		list.add(-2);
+		list.add(-10);
+		list.add(5);
+		list.add(10);
+
+		Client.removeNegatives(list);
+		assertEquals("[5, 10]", list.toString());
+
+	}
+	
+	@Test
+	void testRemovePositives() {
+		list.add(-2);
+		list.add(-10);
+		list.add(5);
+		list.add(10);
+		
+		Client.removePositives(list);
+		assertEquals("[-2, -10]", list.toString());
+	}
+	
+	@Test
+	void testAllOdd() {
+		list.add(1);
+		list.add(3);
+		list.add(5);
+		list.add(7);
+
+		assertFalse(Client.allEven(list));
+	}
 }
